8395487fc9107c849b9894190746693aa911fe3e
jjeong <piusjeong@gmail.com>
2019-01-10 11:16:03 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mmp',
+      version='0.1',
+      description='scripts for microsoft-malware-prediction challenge',
+      url='',
+      author='djeongo',
+      author_email='',
+      license='',
+      packages=['mmp'],
+      zip_safe=False)
+      
